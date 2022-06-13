90e96cf04a5af4daa43436ac0c170ab7554c55fa
George Hawkins <george-hawkins@users.noreply.github.com>
2022-06-13 21:22:21 +0200

    Returned to specifying some values via a .ini file.

---
+++ config.py
@@ -0,0 +1,13 @@
+import configparser
+import os
+
+
+def get_config(config_filename):
+    # Basic interpolation uses the format '%(name)s' - yes, the 's' is really part of it.
+    # So instead the less bizarre extended version, that uses '${name}', is used here.
+    # `os.environ` is passed in so things like '${HOME}' are automatically available.
+    config = configparser.ConfigParser(os.environ, interpolation=configparser.ExtendedInterpolation())
+    config.read(config_filename)
+    return config["DEFAULT"]
+
+
--- launch_ec2_instances.py
+++ launch_ec2_instances.py
@@ -1,56 +1,45 @@
 from collections import Counter
 from time import sleep
-from typing import List, Any
 
 from boto_basics import BotoBasics
 from datetime import datetime, timezone
 from pathlib import Path
 
-# It takes about 30s for a typical instance to start (go from "pending" to "running" and a similar amount of
-# time to go from "running" to "shutting-down" to "terminated" - so 15s is a reasonable polling interval.
+from config import get_config
 from frame_table import FramesTable
 
+# It takes about 30s for a typical instance to start (go from "pending" to "running" and a similar amount of
+# time to go from "running" via "shutting-down" to "terminated"). So 15s seems a reasonable polling interval.
 _POLLING_INTERVAL = 15
 
 basics = BotoBasics()
 
 
-def main():
-    job_id = "bb9138ab-8e5b-4a84-94f5-132de68a010d"
+def _get_latest_image_id(image_name_pattern):
+    image = basics.get_latest_image(image_name_pattern)
 
-    def name(s):
-        return f"render-job-{s}-{job_id}"
-
-    db_name = name("dynamodb")
-    frames_table = FramesTable(basics, db_name)
-    bucket_name = name("bucket")
-    bucket = basics.get_bucket(bucket_name)
-
-    # ------------------------------------------------------------------
-
-    # TODO: move these values into .ini.
-    image = basics.get_latest_image("amzn2-ami-graphics-hvm-*")
-    instance_type = "g4dn.xlarge"
-    # image = basics.get_latest_image("amzn2-ami-kernel-*-hvm-*-x86_64-gp2")
-    # instance_type = "t2.micro"
-
-    # 'Z' is valid for UTC but `fromisoformat` can't handle it - the `datetime` documentation
-    # recommends the 3rd party dateutil for more complete ISO-8601 handling.
+    # Python's `fromisoformat` can handle '+00:00' but not 'Z'. Their documentation
+    # suggests the 3rd party `dateutil` if you need complete ISO-8601 handling.
     created = datetime.fromisoformat(image["CreationDate"].replace("Z", "+00:00"))
     delta = datetime.now(timezone.utc) - created
-    image_id = image["ImageId"]
-
     print(f"Using image {image['Description']} ({image['Name']})")
     print(f"Image created: {created} ({delta.days} days ago)")
 
-    # TODO: pass in "user_data" filename.
-    user_data = Path("user_data").read_text()
-    instance_count = 4
-    security_group_name = "RenderJobWorkerSecurityGroup"
-    iam_instance_profile = "RenderJobWorkerProfile"
+    return image["ImageId"]
 
-    # TODO: name will be the same for all workers - so the best you can do is include the job-id.
-    instance_name = f"render-worker-{job_id}"
+
+def _create_instances(
+    instance_count,
+    instance_name,
+    image_name_pattern,
+    instance_type,
+    security_group_name,
+    iam_instance_profile,
+    user_data_filename
+):
+    image_id = _get_latest_image_id(image_name_pattern)
+
+    user_data = Path(user_data_filename).read_text()
 
     # TODO: can I create the instance without any public IP at all?
     # Answer: no - it needs an IP to access the internet - otherwise you'd have to set up some form of VPN
@@ -71,6 +60,11 @@ def main():
     instance_ids = [instance.instance_id for instance in instances]
     basics.wait_instances_exist(instance_ids)
 
+    return instance_ids
+
+
+# Monitor the instances, track their progress processing the frames and terminate them once completed.
+def _manage_instances(instance_ids, frames_table):
     check_remaining = True
     prev_states = {}
 
@@ -83,7 +77,7 @@ def main():
             print(f"{datetime.now()} Instances: {dict(states_counter)}")
 
             terminated = states_counter["terminated"]
-            if terminated == instance_count:
+            if terminated == len(instance_ids):
                 print("All instances have been terminated")
                 break
 
@@ -97,17 +91,67 @@ def main():
 
         sleep(_POLLING_INTERVAL)
 
-    keys = basics.list_objects(bucket.name, "frames")
-    output_dir = f"frames-{job_id}"
+
+def _download_results(bucket, output_dir):
     Path(output_dir).mkdir(exist_ok=True)
-    print(f"Saving frames to {output_dir}")
+
+    keys = basics.list_objects(bucket.name, "frames")
     for key in keys:
         obj = bucket.Object(key)
         filename = key.split("/")[-1]
         obj.download_file(f"{output_dir}/{filename}")
         print(f"Downloaded {filename}")
 
+
+def _delete_resources():
     # TODO: cleanup once done.
+    pass
+
+
+config = get_config("settings.ini")
+
+
+def main():
+    instance_count = config.getint("instance_count")
+    instance_type = config.get("instance_type")
+    image_name_pattern = config.get("image_name_pattern")
+    security_group_name = config.get("security_group_name")
+    iam_instance_profile = config.get("iam_instance_profile")
+
+    job_id = "bb9138ab-8e5b-4a84-94f5-132de68a010d"
+    user_data_filename = "user_data"
+    output_dir = f"frames-{job_id}"
+
+    # TODO: name will be the same for all workers - so the best you can do is include the job-id.
+    instance_name = f"render-worker-{job_id}"
+
+    def name(s):
+        return f"render-job-{s}-{job_id}"
+
+    db_name = name("dynamodb")
+    frames_table = FramesTable(basics, db_name)
+    bucket_name = name("bucket")
+    bucket = basics.get_bucket(bucket_name)
+
+    # ------------------------------------------------------------------
+
+    instance_ids = _create_instances(
+        instance_count,
+        instance_name,
+        image_name_pattern,
+        instance_type,
+        security_group_name,
+        iam_instance_profile,
+        user_data_filename
+    )
+
+    _manage_instances(instance_ids, frames_table)
+
+    print(f"Saving frames to {output_dir}")
+    _download_results(bucket, output_dir)
+
+    # TODO: maybe this should be called at the level above, i.e. the level at which `bucket` etc. were created.
+    _delete_resources()
 
 
 if __name__ == "__main__":
